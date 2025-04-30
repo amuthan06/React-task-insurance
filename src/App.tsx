5f678387670a0d1ebd0825f36df4a9adf7847977
amuthan06 <148061988+amuthan06@users.noreply.github.com>
2025-04-30 22:29:26 +0530

    Initial Set-up

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-4">
+      <Button>Click me</Button>
+    </div>
+  )
+}
+export default App
